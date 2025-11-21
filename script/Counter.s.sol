19ce5924f2a8a7f9c6f0a2b8c0355d2186a9af48
shubhra tiwary <shubhratiwary5@gmail.com>
2025-11-22 02:33:51 +0530

    minor changes in the deployment script for learning foundry

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -9,11 +9,14 @@ contract CounterScript is Script {
 
     function setUp() public {}
 
-    function run() public {
+    function run() external returns (Counter) {
         vm.startBroadcast();
 
         counter = new Counter();
 
         vm.stopBroadcast();
+
+        return counter;
+
     }
 }
